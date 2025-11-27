9fc165a22dd76f106c6ae8d184c745e36d46c2b8
frasertajima <69366820+frasertajima@users.noreply.github.com>
2025-11-26 20:01:43 -0800

    Delete v28f_b_cudnn_test directory

--- v28f_b_cudnn_test/compile_convergence.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nvfortran -O2 -cuda -gpu=cuda13.0,cc89,ptxinfo -cudaforlibs -lcudnn \
-    common/conv2d_cudnn.cuf \
-    test_convergence.cuf \
-    -o test_convergence
-
-echo "Compilation complete: test_convergence"
